5caabc5e0dd36eef8b87a2e99ff5ee33371a63f3
Chase Ellsworth <chaseme3@gmail.com>
2014-12-15 21:14:31 -0800

    adds photo upload in Angular

--- Client/App/Photos/photosCtrl.js
+++ Client/App/Photos/photosCtrl.js
@@ -8,49 +8,40 @@ angular.module('TravelFilter.PhotosController', [])
 	};
 
 	
-
-
-
-	//   $scope.$watch('files', function() {
-	//     for (var i = 0; i < $scope.files.length; i++) {
-	//       var file = $scope.files[i];
-	//       $scope.upload = $upload.upload({
-	//         url: 'api/photos', // upload.php script, node.js route, or servlet url
-	//         method: 'POST',
-	//         //headers: {'Authorization': 'xxx'}, // only for html5
-	//         //withCredentials: true,
-	//         data: {myObj: $scope.myModelObj},
-	//         file: file, // single file or a list of files. list is only for html5
-	//         //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)        //fileFormDataName: myFile, // file formData name ('Content-Disposition'), server side request form name
-	//                                     // could be a list of names for multiple files (html5). Default is 'file'
-	//         //formDataAppender: function(formData, key, val){}  // customize how data is added to the formData. 
-	//                                                             // See #40#issuecomment-28612000 for sample code
-
-	//       }).progress(function(evt) {
-	//         console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
-	//         console.log(evt);
-	//         console.log(evt.config);
-	//         console.log(evt.config.data);
-	//         console.log(evt.config.file);
-	//       }).success(function(data, status, headers, config) {
-	//         // file is uploaded successfully
-	//         console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
-	//       });
-	//       //.error(...)
-	//       //.then(success, error, progress); // returns a promise that does NOT have progress/abort/xhr functions
-	//       //.xhr(function(xhr){xhr.upload.addEventListener(...)}) // access or attach event listeners to 
-	//                                                               //the underlying XMLHttpRequest
-	//     }
-	// });
-	//     /* alternative way of uploading, send the file binary with the file's content-type.
-	//        Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
-	//        It could also be used to monitor the progress of a normal http post/put request. 
-	//        Note that the whole file will be loaded in browser first so large files could crash the browser.
-	//        You should verify the file size before uploading with $upload.http().
-	//     */
-	//     // $scope.upload = $upload.http({...})  // See 88#issuecomment-31366487 for sample code.
-
-
+	$scope.onFileSelect = function(files){
+	    
+    console.log($scope.title + Auth.currentUser);
+    for(var i = 0; i < files.length; i++){
+      var file = files[i];
+                              console.log(file);
+      $scope.upload = $upload.upload({
+        method: 'POST',
+        url:  'api/photos/',
+        data: {
+          prompt_id: $scope.imageId,
+          user_id:   Auth.currentUser      
+        },
+        file: file
+      })
+      .progress(function(evt){
+        // console.log(evt);
+        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total))
+      })
+      .success(function(data, status, headers, config){
+        Edit.imageId.push(data);
+        $scope.imageNamesToDisplay = Edit.imageId;
+        console.log($scope.imageNamesToDisplay);
+        console.log("Edit.imageId is: ", Edit.imageId);
+                              console.log(status);
+                              console.log(headers);
+                              console.log(config);
+        // $state.reload();                             ///fixes the issue of the content disappearing. What is the side affect of removing it??
+      })
+      .error(function(error){
+        console.log('ERROR: '. error);
+      })
+    }
+	}
 
 	//GET PHOTOS
 	$scope.name = "chase";
